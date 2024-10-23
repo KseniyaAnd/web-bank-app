550e7f85f7a92ebd9b06901e4bf6e58bc6351216
Kseniya Andrusenka <kandrusenka@ibagroup.eu>
2024-10-23 18:51:52 +0300

    feat: add header

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("JavaScript is working!");
+const navTitle = document.querySelector(".nav-title");
+const navList = document.querySelector(".nav-list");
+
+// Открываем меню при клике на заголовок
+navTitle.addEventListener('click', (event) => {
+    event.stopPropagation(); // Останавливаем распространение события
+    navTitle.style.opacity = 0;
+    navList.classList.add("show-nav");
+});
+
+// Закрываем меню при клике вне меню
+document.addEventListener('click', () => {
+    navTitle.style.opacity = 1;
+    navList.classList.remove("show-nav");
+});
+
+// Чтобы предотвратить закрытие меню при клике внутри него
+navList.addEventListener('click', (event) => {
+    event.stopPropagation(); // Останавливаем клик внутри меню от распространения
 });
