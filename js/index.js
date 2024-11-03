94b2c4bb2a3fecbb7092735cbe647ed9933e8f94
Kseniya Andrusenka <kandrusenka@ibagroup.eu>
2024-11-03 16:33:10 +0300

    feat: add log in function and adding new users

--- js/index.js
+++ js/index.js
@@ -3,14 +3,17 @@
 
 let Users = new Map([
     ['Jonas Schmedtmann', {
+        username: 'JS',
         movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
         pin: 1111,
     }],
     ['Jessica Davis', {
+        username: 'JD',
         movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
         pin: 2222,
     }],
     ['Steven Thomas Williams', {
+        username: 'STW',
         movements: [200, -200, 340, -300, -20, 50, 400, -460],
         pin: 3333,
     }]
@@ -18,7 +21,6 @@ let Users = new Map([
 
 
 const header = document.querySelector('.header');
-const labelWelcome = document.querySelector('.welcome');
 const loginForm = document.querySelector('.login');
 const signupForm = document.querySelector('.signup');
 const loginLink = document.querySelector('.login-link');
@@ -29,21 +31,42 @@ const inputSignupFullname = document.querySelector('.signup__input--fullname');
 const inputSignupPin = document.querySelector('.signup__input--pin');
 const signupBtn = document.querySelector(".signup-btn");
 
+
 const clearInput = (input) => {
     input.value = '';
 }
 
-function toggleForms(activeForm, inactiveForm, usernameInput, pinInput) {
-    activeForm.classList.toggle('opacity-zero');
-    activeForm.classList.toggle('z-index-1');
-    inactiveForm.classList.toggle('opacity-one');
-    inactiveForm.classList.toggle('z-index-1');
+const toggleForms = (activeForm, inactiveForm, usernameInput, pinInput) => {
+    inactiveForm.classList.add('opacity-zero');
+    inactiveForm.classList.remove('opacity-one');
+    inactiveForm.classList.remove('z-index-1');
+
+    activeForm.classList.remove('opacity-zero');
+    activeForm.classList.add('opacity-one');
+    activeForm.classList.add('z-index-1');
+
     clearInput(usernameInput);
     clearInput(pinInput);
 }
 
+const toggleHeader = () => {
+    if (currentAccount) {
+        header.classList.add('opacity-one');
+        header.classList.remove('opacity-zero');
+    } else {
+        header.classList.add('opacity-zero');
+        header.classList.remove('opacity-one');
+    }
+}
+
+const validatePin = (pin) => {
+    const pinString = String(pin);
+    return pinString.length >= 4 && /^\d+$/.test(pinString);
+};
+
 const addUser = (fullname, pin) => {
     const newUser = {
+        username: createUsername(fullname),
         movements: [],
         pin: pin,
     };
@@ -53,37 +76,28 @@ const addUser = (fullname, pin) => {
         console.log(`Account for ${fullname} has been added.`);
         clearInput(inputSignupFullname);
         clearInput(inputSignupPin);
+        currentAccount = newUser;
+        signupForm.classList.add('opacity-zero');
+        signupForm.classList.remove('opacity-one');
+        signupForm.classList.remove('z-index-1');
+        toggleHeader();
+
     } else {
         console.log(`Account for ${fullname} already exists.`);
     }
 };
 
-const createUsernames = function (users) {
-    users.forEach(function (users) {
-        users.fullname = users.owner
-            .toLowerCase()
-            .split(' ')
-            .map(name => name[0])
-            .join('');
-    });
+const createUsername = (fullname) => {
+    return fullname
+        .toLowerCase()
+        .split(' ')
+        .map(name => name[0])
+        .join('');
 };
 
 let currentAccount;
-
-if (!currentAccount) {
-    header.classList.add('opacity-zero');
-    loginForm.classList.add('opacity-one');
-    loginForm.classList.add('z-index-1');
-    signupForm.classList.add('opacity-zero');
-}
-
-if (currentAccount) {
-    loginForm.classList.add('opacity-zero');
-    signupForm.classList.add('opacity-zero');
-    header.classList.add('opacity-one');
-    labelWelcome.classList.add('opacity-one');
-    labelWelcome.textContent = `Welcome back`;
-}
+toggleHeader();
+toggleForms(loginForm, signupForm, inputLoginUsername, inputLoginPin);
 
 loginLink.addEventListener('click', () => {
     toggleForms(loginForm, signupForm, inputLoginUsername, inputLoginPin);
@@ -97,5 +111,12 @@ signupBtn.addEventListener('click', (e) => {
     e.preventDefault();
     const fullname = inputSignupFullname.value;
     const pin = Number(inputSignupPin.value);
-    addUser(fullname, pin);
+    if (validatePin(pin)) {
+        addUser(fullname, pin);
+    } else {
+        inputSignupPin.classList.add('input-error');
+        setTimeout(() => {
+            inputSignupPin.classList.remove('input-error');
+        }, 1000);
+    }
 });
